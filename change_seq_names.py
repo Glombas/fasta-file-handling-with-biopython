6c9c2f42ce5858025481ca4dbeda4e237c772a29
Marek Glombik <42178209+Glombas@users.noreply.github.com>
2024-11-12 17:44:46 +0000

    Update comments to the code

--- change_seq_names.py
+++ change_seq_names.py
@@ -5,12 +5,17 @@ Created on Tue Sep 24 12:31:30 2024
 
 @author: glombik
 """
-from Bio import SeqIO 
+#add SeqIO, made to handle fasta sequences
+from Bio import SeqIO
+#add argparse to pass arguments when running the script (see below)
 import argparse
 
+#define a function to change the sequence names/headers
 def adjust_fasta_names(input_file, output_file, list_of_names):    
+#start with an empty dictionary for sequence names
     name_updates = {}
-    
+
+#open the file with list of names (tab separated), read it line by line and create an entry into the dictionary keeping the connection between the current (old) name and the new name
     with open(list_of_names) as file:
         for line in file:
             line = line.strip()
@@ -18,9 +23,11 @@ def adjust_fasta_names(input_file, output_file, list_of_names):
             old_name = read[0].strip()
             updated_name = read[1].strip()
             name_updates[old_name] = updated_name
-            
+
+    #open the output file for writing            
     with open(output_file,'w') as adjusted:
-        
+
+    #open the input file with sequences, strip the name (header) and if there is a match to the dictionary, update the name with a new one.
         with open(input_file) as fasta_in:
             for record in SeqIO.parse(fasta_in,'fasta'):
                 record_id = record.id.strip()
@@ -42,5 +49,5 @@ if __name__ == "__main__":
 # Parse the arguments
 args = parser.parse_args()
 
-# Call the function with provided arguments
+# Call the function with provided arguments (see above)
 adjust_fasta_names(args.input, args.output, args.names)
